f806637452518bb5852a91c454d6413ee642e850
Oscar Sandford <oscarsandford@gmail.com>
2022-08-31 18:07:27 -0700

    Refactored project base.

---
+++ spfe/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div >
+      <Nav/>
+        <div className='spe-header'>
+          Hello worlds
+        </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ spfe/src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div>
+      this is the nav
+    </div>
+  );
+}
+
+export default Nav;
